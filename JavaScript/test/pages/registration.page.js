d9ded30c30d8c03b53a307c1bb66b493bb2bca08
David Nekovář <nekovard@gmail.com>
2017-12-15 16:57:08 +0100

    strenght indicator has correct text and class for different passwords
    my got who made that indicator...

--- JavaScript/test/pages/registration.page.js
+++ JavaScript/test/pages/registration.page.js
@@ -2,24 +2,24 @@ import ThankYou from './thank-you.page';
 
 class Registration {
     // page elements
-    get lastName()          { return $("[name='last_name']"); }
-    get firstName()         { return $("[name='first_name']"); }
-    get maritalStatus()     { return $$(".radio_wrap input[type='radio']"); }
-    get hobby()             { return $$(".radio_wrap input[type='checkbox']"); }
-    get country()           { return $("#dropdown_7"); }
-    get month()             { return $("#mm_date_8"); }
-    get day()               { return $("#dd_date_8"); }
-    get year()              { return $("#yy_date_8"); }
-    get phone()             { return $("#phone_9"); }
-    get username()          { return $("#username"); }
-    get email()             { return $("[name='e_mail']"); }
-    get picture()           { return $("#profile_pic_10"); }
-    get aboutYourself()     { return $("#description"); }
-    get password()          { return $("#password_2"); }
-    get confirmPassword()   { return $("#confirm_password_password_2"); }
-    get passwordStrength()  { return $("#piereg_passwordStrength"); }
-    get submit()            { return $("[name='pie_submit']"); }
-    
+    get lastName() { return $("[name='last_name']"); }
+    get firstName() { return $("[name='first_name']"); }
+    get maritalStatus() { return $$(".radio_wrap input[type='radio']"); }
+    get hobby() { return $$(".radio_wrap input[type='checkbox']"); }
+    get country() { return $("#dropdown_7"); }
+    get month() { return $("#mm_date_8"); }
+    get day() { return $("#dd_date_8"); }
+    get year() { return $("#yy_date_8"); }
+    get phone() { return $("#phone_9"); }
+    get username() { return $("#username"); }
+    get email() { return $("[name='e_mail']"); }
+    get picture() { return $("#profile_pic_10"); }
+    get aboutYourself() { return $("#description"); }
+    get password() { return $("#password_2"); }
+    get confirmPassword() { return $("#confirm_password_password_2"); }
+    get passwordStrength() { return $("#piereg_passwordStrength"); }
+    get submit() { return $("[name='pie_submit']"); }
+
     /**
      * opens this page object in browser
      */
@@ -32,17 +32,22 @@ class Registration {
      * @param {Number} str number indicating the strength 0 (none) - 4 (strong)
      */
     comparePasswordStrength(str) {
-        const strTexts =    { 0: "Strength Indicator", 1: "Very week", 2: "Week", 3: "Medium", 4: "Strong" };
-        const strClasses =  { 0: "piereg_pass", 1: "piereg_pass_v_week", 2: "piereg_pass_week", 3: "piereg_pass_medium", 4: "piereg_pass_strong" };
-        
+        const strTexts = { 0: "Strength Indicator", 1: "Very weak", 2: "Weak", 3: "Medium", 4: "Strong" };
+        const strClasses = { 0: "piereg_pass", 1: "piereg_pass_v_week", 2: "piereg_pass_week", 3: "piereg_pass_medium", 4: "piereg_pass_strong" };
+
+        // indicator is not changing fast enough, it needs be tabed several times to show correct value
+        // i vould report it as low priority and severity bug...
+        // TODO: after fix of indicator reload, change this tabing maddnes
+        this.password.addValue("\uE004");
+
         const sameText = strTexts[str] == this.passwordStrength.getText();
         const sameClass = strClasses[str] == this.passwordStrength.getAttribute('class');
-        
+
         return sameText && sameClass;
     }
 
     fillForm() {
-        
+
     }
 }
 
--- JavaScript/test/registration.test.js
+++ JavaScript/test/registration.test.js
@@ -1,12 +1,27 @@
 import RegistrationPage from './pages/registration.page';
-import { expect } from 'chai'; 
+import { expect } from 'chai';
 
 describe('registration page', () => {
     beforeEach(() => {
-        RegistrationPage.open();  
+        RegistrationPage.open();
     });
 
-    it('strenght indicator is showing no strength by default', () => {      
+    it('strenght indicator is showing no strength by default', () => {
         expect(RegistrationPage.comparePasswordStrength(0)).to.be.true;
     });
+
+    it('strenght indicator has correct text and class for different passwords', () => {
+        const passwords = ['123', '1asd45fgh6', 'as369d741lok', 'Mas#159oLp*']
+
+        for (let index = 0; index < passwords.length; index++) {
+            RegistrationPage.password.setValue(passwords[index])
+
+            const value = browser.waitUntil(() => {
+                return RegistrationPage.comparePasswordStrength(index + 1)
+            }, 3000, 'expected class and text change after 1s')
+
+            expect(value).to.be.true;
+        }
+    });
+
 });
